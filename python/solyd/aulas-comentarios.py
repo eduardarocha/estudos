5596e0e5044c7d9f0c6db38860b8d5bceadeccb2
Edudda <eduardagsrocha@pm.me>
2021-12-27 19:43:25 -0400

    27 dez. 2021
    python

---
+++ python/solyd/aulas-comentarios.py
@@ -0,0 +1,36 @@
+# Aula 1 - Introdução ao Python e Configurando o Ambiente:
+'''
+Instalação do Python: https://www.python.org/downloads
+'''
+
+# Aula 2 - Variáveis, tipos, entrada, saída e operadores matemáticos:
+'''
+v = "valor" : variável = valor sendo função, str, int, float
+print(v) : Imprime o valor de variáveis, funções.
+    \n : Quebra de linha.
+    \t : Tab; espaço em branco.
+    , : Transforma em string e junta valores colocando um espaço entre eles.
+    + : Soma valores de tipos iguais.
+type(v) : Imprime o tipo do valor de uma variável.
+str(v) : Converte valores de variáveis para tipo string.
+input(v) : Exmprime uma entrada de valor
+
++ : Soma.
+- : Subtração.
+* : Multiplicação.
+/ : Divisão.
+** : Potência.
+**(1/2) : Raiz quadrada.
+'''
+# Atividade:
+'''
+nome = input("Qual o seu nome?\n")
+cpf = input("Qual o seu CPF?\n")
+endereco = input("Qual o seu endereço?\n")
+idade = input("Qual a sua idade?\n")
+altura = input("Qual a sua altura?\n")
+telefone = input("Qual o seu telefone?\n")
+
+print("\nSEU RELATÓRIO\nNome:",nome+"\nCPF:",cpf+"\nEndereço:,",endereco+"\nIdade:",idade,"anos"+"\nAltura:",altura,"metros"+"\nTelefone:",telefone)
+'''
+#:
