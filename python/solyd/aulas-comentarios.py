127db8260a2a0b91988fa12bd795b8e455643ca0
Edudda <eduardagsrocha@pm.me>
2021-12-30 19:40:45 -0400

    30 dez. 2021

--- python/solyd/aulas-comentarios.py
+++ python/solyd/aulas-comentarios.py
@@ -98,19 +98,67 @@ break : Finaliza um processo de repetição.
 '''
 # Atividade:
 '''
-pessoas = int(input("Quantas pessoas serão convidadas para a festa?\n"))
-lista = []
-
-while pessoas >= 1:     #for i in range(int(pessoas)):
-    nome = input("Este convite #"+str(pessoas)+" pertence à: ")
+pessoas = int(input("Quantas pessoas convidadas?\n"))
+convites = []
+while pessoas >= 1:
+    nome = input("Convite #"+str(pessoas)+" para: ")
+    convites.append(nome)
     pessoas -= 1
-    lista.append(nome)
-
 print("Lista de convidados:")
-for convidado in lista:
-    print(convidado)
+for nome in convites:
+    print(nome)
 '''
+# https://www.pythonprogressivo.net/2018/06/Como-Validar-Dados-em-Python-Exemplos-Codigo-Comentado.html
+'''
+nota = float(input("Registre a nota: "))
+while (nota < 0) or (nota > 10):
+    nota = float(input("Valor inválido! Tente novamente: "))
+print("Nota",str(nota),"registrada.")
+'''
+'''
+user = input("User: ")
+senha = input("Senha: ")
+while senha == user:
+    print("Nome de usuário e senha iguais. Tente novamente:")
+    senha = input("Senha: ")
+print("Acesso confirmado.")
+'''
+'''
+nome = input("\nNome: ")
+idade = int(input("Idade: "))
+salario = float(input("Salário: "))
+sexo = input("Sexo: ")
+estado_civil = input("Estado Civil: ")
+while len(nome) <= 3:
+    nome = input("\nNome inválido. Tente novamente:\nNome: ")
+while (idade <= 0) or (idade >= 150):
+    idade = int(input("\nIdade impossível. Tente novamente:\nIdade: "))
+while salario < 0:
+    salario = float(input("\nValor inválido. Tente novamente:\nSalário: "))
+while sexo != ("f" or "m"):
+    sexo = input("\nSexo inválido. Tente novamente:\nSexo: ")
+while estado_civil != ("s" or "c" or "v" or "d"):
+    estado_civil = input("\nEstado Civil inválido. Tente novamente:\nEstado Civil: ")
+print("\nInformações registradas."+nome+"\n"+str(idade)+"\n"+str(salario)+"\n"+sexo+"\n"+estado_civil)
+'''
+'''
+a = int(input("População A: "))
+b = int(input("População B: "))
+taxa_a = float(input("Taxa da polulação A: "))
+taxa_b = float(input("Taxa da população B: "))
+ano = 0
+while a < b:
+    ano += 1
+    crescimento_a = (a * (taxa_a/100))      # a *= 1 + (taxa_a/100)
+    a += crescimento_a
+    crescimento_b = (b * (taxa_b/100))      # B *= 1 + (taxa_b/100)
+    b += crescimento_b
+    print("\nAno:",str(ano))
+    print("Cidade A:",str(a))
+    print("Cidade B:",str(b))
+'''
+
 # Aula 6 - Tuplas, dicionários e conjuntos:
 '''
-
+ 
 '''
